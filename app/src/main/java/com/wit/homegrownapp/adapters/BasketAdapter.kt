b691a1cacd584c293fdd1a7c7caf2be4876d31f2
Rob Fox <60361504+foxxxxxxx7@users.noreply.github.com>
2023-04-20 00:53:17 +0100

    Increase and Decrease Quantities

--- app/src/main/java/com/wit/homegrownapp/adapters/BasketAdapter.kt
+++ app/src/main/java/com/wit/homegrownapp/adapters/BasketAdapter.kt
@@ -1,40 +1,48 @@
 package com.wit.homegrownapp.adapters
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.CircleCrop
-import com.bumptech.glide.load.resource.bitmap.CenterCrop
-import com.bumptech.glide.request.RequestOptions
 import com.wit.homegrownapp.databinding.CardBasketItemBinding
 import com.wit.homegrownapp.model.BasketItemModel
 
+interface BasketListener {
+    fun onBasketItemClick(basketItem: BasketItemModel)
+    fun onIncreaseQuantityClick(basketItem: BasketItemModel)
+    fun onDecreaseQuantityClick(basketItem: BasketItemModel)
+}
 
-class BasketAdapter :
+
+class BasketAdapter(private val listener: BasketListener) :
     ListAdapter<BasketItemModel, BasketAdapter.BasketViewHolder>(BasketItemModelDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BasketViewHolder {
         val binding =
             CardBasketItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return BasketViewHolder(binding)
+        return BasketViewHolder(binding, listener)
     }
 
     override fun onBindViewHolder(holder: BasketViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
 
-    class BasketViewHolder(private val binding: CardBasketItemBinding) :
+    class BasketViewHolder(private val binding: CardBasketItemBinding, private val listener: BasketListener) :
         RecyclerView.ViewHolder(binding.root) {
+
         fun bind(item: BasketItemModel) {
             binding.basketItem = item
             binding.imageIcon.setImageResource(item.icon)
             binding.executePendingBindings()
+            binding.root.setOnClickListener { listener.onBasketItemClick(item) }
+
+
+            binding.plusButton.setOnClickListener { listener.onIncreaseQuantityClick(item) }
+            binding.minusButton.setOnClickListener { listener.onDecreaseQuantityClick(item) }
         }
     }
+
 
     class BasketItemModelDiffCallback : DiffUtil.ItemCallback<BasketItemModel>() {
         override fun areItemsTheSame(oldItem: BasketItemModel, newItem: BasketItemModel): Boolean {
--- app/src/main/java/com/wit/homegrownapp/ui/basket/BasketFragment.kt
+++ app/src/main/java/com/wit/homegrownapp/ui/basket/BasketFragment.kt
@@ -9,11 +9,12 @@ import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.wit.homegrownapp.R
 import com.wit.homegrownapp.adapters.BasketAdapter
+import com.wit.homegrownapp.adapters.BasketListener
 import androidx.fragment.app.viewModels
 import com.wit.homegrownapp.databinding.FragmentBasketBinding
 import com.wit.homegrownapp.model.BasketItemModel
 
-class BasketFragment : Fragment() {
+class BasketFragment : Fragment(), BasketListener {
 
     private var _binding: FragmentBasketBinding? = null
     private val binding get() = _binding!!
@@ -35,7 +36,7 @@ class BasketFragment : Fragment() {
     }
 
     private fun setupRecyclerView() {
-        basketAdapter = BasketAdapter()
+        basketAdapter = BasketAdapter(this)
         binding.basketRecyclerView.apply {
             layoutManager = LinearLayoutManager(requireContext())
             adapter = basketAdapter
@@ -53,6 +54,22 @@ class BasketFragment : Fragment() {
     private fun updateTotalPrice(basketItems: List<BasketItemModel>) {
         val totalPrice = basketItems.sumOf { it.price * it.quantity }
         binding.totalPriceTextView.text = getString(R.string.total_price, totalPrice)
+    }
+
+    override fun onIncreaseQuantityClick(basketItem: BasketItemModel) {
+        basketViewModel.updateBasketItemQuantity(basketItem.biid, basketItem.quantity + 1)
+    }
+
+    override fun onDecreaseQuantityClick(basketItem: BasketItemModel) {
+        if (basketItem.quantity > 1) {
+            basketViewModel.updateBasketItemQuantity(basketItem.biid, basketItem.quantity - 1)
+        } else {
+            basketViewModel.removeBasketItem(basketItem.biid)
+        }
+    }
+
+    override fun onBasketItemClick(basketItem: BasketItemModel) {
+        // Handle click event here
     }
 
     override fun onDestroyView() {
