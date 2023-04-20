1dcee0db365b49270e6246c889ef72497cec021f
Rob Fox <60361504+foxxxxxxx7@users.noreply.github.com>
2023-04-20 02:44:18 +0100

    Update SwipeToAddToBasketCallback.kt

--- app/src/main/java/com/wit/homegrownapp/utils/SwipeToAddToBasketCallback.kt
+++ app/src/main/java/com/wit/homegrownapp/utils/SwipeToAddToBasketCallback.kt
@@ -16,7 +16,8 @@ import com.wit.homegrownapp.adapters.ProductAdapter
 abstract class SwipeToAddToBasketCallback(context: Context) :
     ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
 
-    private val addToBasketIcon = ContextCompat.getDrawable(context, R.drawable.baseline_shopping_basket_24)
+    private val addToBasketIcon =
+        ContextCompat.getDrawable(context, R.drawable.baseline_shopping_basket_24)
     private val intrinsicWidth = addToBasketIcon?.intrinsicWidth
     private val intrinsicHeight = addToBasketIcon?.intrinsicHeight
     private val background = ColorDrawable()
@@ -24,8 +25,7 @@ abstract class SwipeToAddToBasketCallback(context: Context) :
     private val clearPaint = Paint().apply { xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR) }
 
     override fun getMovementFlags(
-        recyclerView: RecyclerView,
-        viewHolder: RecyclerView.ViewHolder
+        recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder
     ): Int {
         if (!(viewHolder as ProductAdapter.MainHolder).readOnlyRow) return 0
         return super.getMovementFlags(recyclerView, viewHolder)
@@ -40,9 +40,15 @@ abstract class SwipeToAddToBasketCallback(context: Context) :
     }
 
     override fun onChildDraw(
-        c: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder,
-        dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean
+        c: Canvas,
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        dX: Float,
+        dY: Float,
+        actionState: Int,
+        isCurrentlyActive: Boolean
     ) {
+
         val itemView = viewHolder.itemView
         val itemHeight = itemView.bottom - itemView.top
         val isCanceled = dX == 0f && !isCurrentlyActive
@@ -50,34 +56,31 @@ abstract class SwipeToAddToBasketCallback(context: Context) :
         if (isCanceled) {
             clearCanvas(
                 c,
-                itemView.right + dX,
+                itemView.left + dX,
                 itemView.top.toFloat(),
-                itemView.right.toFloat(),
+                itemView.left.toFloat(),
                 itemView.bottom.toFloat()
             )
             super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
             return
         }
 
-        // Draw the green add to basket background
+        // Draw the blue edit background
         background.color = backgroundColor
         background.setBounds(
-            itemView.right + dX.toInt(),
-            itemView.top,
-            itemView.right,
-            itemView.bottom
+            itemView.left + dX.toInt(), itemView.top, itemView.left, itemView.bottom
         )
         background.draw(c)
 
-        // Calculate position of add to basket icon
-        val addToBasketIconTop = itemView.top + (itemHeight - intrinsicHeight!!) / 2
-        val addToBasketIconMargin = (itemHeight - intrinsicHeight) / 2
-        val addToBasketIconLeft = itemView.right - addToBasketIconMargin - intrinsicWidth!!
-        val addToBasketIconRight = itemView.right - addToBasketIconMargin
-        val addToBasketIconBottom = addToBasketIconTop + intrinsicHeight
+        // Calculate position of Edit icon
+        val editIconTop = itemView.top + (itemHeight - intrinsicHeight!!) / 2
+        val editIconMargin = (itemHeight - intrinsicHeight) / 2
+        val editIconLeft = itemView.right - editIconMargin - intrinsicWidth!! - 810
+        val editIconRight = itemView.right - editIconMargin - 810
+        val editIconBottom = editIconTop + intrinsicHeight
 
-        // Draw the add to basket icon
-        addToBasketIcon?.setBounds(addToBasketIconLeft, addToBasketIconTop, addToBasketIconRight, addToBasketIconBottom)
+        // Draw the edit icon
+        addToBasketIcon?.setBounds(editIconLeft, editIconTop, editIconRight, editIconBottom)
         addToBasketIcon?.draw(c)
 
         super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
