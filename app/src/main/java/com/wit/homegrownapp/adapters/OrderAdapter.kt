18d23c42dcc82654a35901b9a872b034af065245
Rob Fox <60361504+foxxxxxxx7@users.noreply.github.com>
2023-04-28 19:00:41 +0100

    Update OrderAdapter.kt

--- app/src/main/java/com/wit/homegrownapp/adapters/OrderAdapter.kt
+++ app/src/main/java/com/wit/homegrownapp/adapters/OrderAdapter.kt
@@ -2,10 +2,12 @@ package com.wit.homegrownapp.adapters
 
 import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 import com.wit.homegrownapp.R
 import com.wit.homegrownapp.databinding.CardOrderBinding
 import com.wit.homegrownapp.firebase.FirebaseDBManager
@@ -51,6 +53,13 @@ class OrderAdapter(private val listener: OrderListener) :
             updateStatusIndicator(item.status)
             binding.executePendingBindings()
             binding.root.setOnClickListener { listener.onOrderClick(item) }
+
+            // Hide accept and decline buttons if the current user is the one who placed the order
+            if (item.uid == FirebaseAuth.getInstance().currentUser?.uid) {
+                setButtonsVisibility(View.GONE)
+            } else {
+                setButtonsVisibility(View.VISIBLE)
+            }
         }
 
         private fun updateStatusIndicator(status: String) {
@@ -58,19 +67,26 @@ class OrderAdapter(private val listener: OrderListener) :
                 "pending" -> {
                     binding.statusIndicator.setImageResource(R.drawable.ic_menu_about)
                     binding.statusIndicator.setBackgroundColor(Color.parseColor("#DA530A"))
+                    setButtonsVisibility(View.VISIBLE) // Show buttons
                 }
                 "accepted" -> {
                     binding.statusIndicator.setImageResource(R.drawable.ic_check_mark)
                     binding.statusIndicator.setBackgroundColor(Color.parseColor("#008450"))
+                    setButtonsVisibility(View.GONE) // Hide buttons
                 }
                 "declined" -> {
                     binding.statusIndicator.setImageResource(R.drawable.ic_cross_mark)
                     binding.statusIndicator.setBackgroundColor(Color.parseColor("#B81D13"))
+                    setButtonsVisibility(View.GONE) // Hide buttons
                 }
             }
         }
-    }
 
+        private fun setButtonsVisibility(visibility: Int) {
+            binding.tickButton.visibility = visibility
+            binding.crossButton.visibility = visibility
+        }
+    }
 
     class OrderModelDiffCallback : DiffUtil.ItemCallback<OrderModel>() {
         override fun areItemsTheSame(oldItem: OrderModel, newItem: OrderModel): Boolean {
